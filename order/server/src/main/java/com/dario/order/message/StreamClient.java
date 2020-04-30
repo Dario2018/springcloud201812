2aaf709462d622a77fdfb95f244166211be8d5e0
chendeda.zqgame <chendeda@2>
2020-04-30 13:56:59 +0800

    添加springcloud stream 组件使用注释

--- order/server/src/main/java/com/dario/order/message/StreamClient.java
+++ order/server/src/main/java/com/dario/order/message/StreamClient.java
@@ -6,8 +6,15 @@ import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.SubscribableChannel;
 
 /*
- *
- *
+ *StpringCloud Stream 封装与消息队列的交互
+ * 1.StreamClient.class 为 stream 组件
+ * 2.在 test中AmqpTemplate 直接与中间件交互，那操作也可以
+ * 3.接口存在两种类型的通道
+ * 各个通道名字不能一样
+ * 如果没有指定value，则
+ * 方法名为通道的名字
+ * 需要在配置yml 配置通道
+ * 的绑定（bingder）
  * */
 public interface StreamClient {
 
